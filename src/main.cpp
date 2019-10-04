f58871db765b00900b210bf040b1bef18a1c526d
Mike Skutta <mskutta@onenorth.com>
2019-10-03 19:12:18 -0500

    Added additional environment configuration.  Only turn on display when tripped.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
-//#define TRIGGER_1 1
-//#define TRIGGER_2 1
-//#define TRIGGER_3 1
-//#define TRIGGER_4 1
+#if !(defined(ESP_NAME))
+  #define ESP_NAME "trigger" 
+#endif
+
+#if !(defined(DISPLAY_128X32) || defined(DISPLAY_128X64))
+  #define DISPLAY_128X64 1 
+#endif
+
+#if !(defined(MAX_RANGE))
+  #define MAX_RANGE 1200 
+#endif
+
+// Uncomment this line to use long range mode. This
+// increases the sensitivity of the sensor and extends its
+// potential range, but increases the likelihood of getting
+// an inaccurate reading because of reflections from objects
+// other than the intended target. It works best in dark
+// conditions.
+
+//#define LONG_RANGE
+
+// Uncomment ONE of these two lines to get
+// - higher speed at the cost of lower accuracy OR
+// - higher accuracy at the cost of lower speed
+
+//#define HIGH_SPEED
+//#define HIGH_ACCURACY
 
 #include <Arduino.h>
 
@@ -29,8 +52,10 @@
 // Sensor
 #include <VL53L0X.h>
 
+// Median for Sensor
+#include <RunningMedian.h>
+
 /* Constants */
-const char* ESP_NAME = "trigger";
 const unsigned long RUN_INTERVAL = 100;
 
 /* Variables */
@@ -40,6 +65,7 @@ int minRange = 0;
 
 unsigned int count = 0;
 unsigned long nextRun = 0;
+unsigned long displayTimeout = 0;
 
 /* Display */
 SSD1306AsciiWire oled;
@@ -50,6 +76,7 @@ char hostname[32] = {0};
 
 /* VL53L0X */
 VL53L0X sensor;
+RunningMedian samples = RunningMedian(9);
 
 /* OSC */
 WiFiUDP Udp;
@@ -92,7 +119,11 @@ void setup()
   delay(1000);
 
   /* Display */
+#if defined DISPLAY_128X64
   oled.begin(&Adafruit128x64, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
+#else
+  oled.begin(&Adafruit128x32, 0x3C);
+#endif
   oled.setFont(System5x7);
   oled.setScrollMode(SCROLL_MODE_AUTO);
   oled.clear();
@@ -116,9 +147,11 @@ void setup()
   oled.print(WiFi.localIP());
   oled.print(F(":"));
   oled.println(Udp.localPort());
+#if defined DISPLAY_128X64
   oled.print(F("  "));
   oled.println(WiFi.macAddress());
-  
+#endif
+
   /* OTA */
   ArduinoOTA.setHostname(hostname);
   ArduinoOTA.onStart([]() {
@@ -164,6 +197,7 @@ void setup()
   qLabIp = MDNS.IP(0);
   qLabPort = MDNS.port(0);
 
+#if defined DISPLAY_128X64
   oled.println(qLabHostname);
   oled.print(F("  "));
   oled.print(qLabIp);
@@ -171,10 +205,26 @@ void setup()
   oled.println(qLabPort);
   oled.print(F("  "));
   oled.println(QLAB_MESSAGE);
+#endif
 
   /* Sensor */
   sensor.init();
   sensor.setTimeout(500);
+#if defined LONG_RANGE
+  // lower the return signal rate limit (default is 0.25 MCPS)
+  sensor.setSignalRateLimit(0.1);
+  // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+#endif
+
+#if defined HIGH_SPEED
+  // reduce timing budget to 20 ms (default is about 33 ms)
+  sensor.setMeasurementTimingBudget(20000);
+#elif defined HIGH_ACCURACY
+  // increase timing budget to 200 ms
+  sensor.setMeasurementTimingBudget(200000);
+#endif
   sensor.startContinuous();
 
   // Calibrate
@@ -184,6 +234,7 @@ void setup()
   int range = 0;
   while((range = sensor.readRangeContinuousMillimeters()) > 1200) {
     oled.printf("Calibrate: %u\r", calibrateCount);
+    ArduinoOTA.handle();
     delay(100);
     calibrateCount++;
   }
@@ -194,6 +245,9 @@ void setup()
   /* LED */
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+
+  oled.ssd1306WriteCmd(SSD1306_DISPLAYOFF);
+  //oled.setContrast(0);
 }
 
 void loop()
@@ -206,22 +260,27 @@ void loop()
   nextRun = millis() + RUN_INTERVAL;
 
   int range = sensor.readRangeContinuousMillimeters();
-  Serial.println(range);
-  if (sensor.timeoutOccurred() || range > 1200 ) { 
+  if (sensor.timeoutOccurred()) { 
     return;
   }
 
+  // Filter Noise
+  //samples.add(range);
+  //range = samples.getMedian();
+
   // Detect if tripped
-  if ((range < (maxRange - 200)) && !tripped) {
+  if ((range < (maxRange - 100)) && !tripped) {
     sendQLabOSCMessage(QLAB_MESSAGE);
     digitalWrite(LED_BUILTIN, LOW);
     tripped = true;
+    oled.ssd1306WriteCmd(SSD1306_DISPLAYON);
     minRange = range; // Reset minimum range
     count++;
   } 
-  else if (range > (minRange + 200) && tripped) {
+  else if (range > (minRange + 100) && tripped) {
     digitalWrite(LED_BUILTIN, HIGH);
     tripped = false;
+    oled.ssd1306WriteCmd(SSD1306_DISPLAYOFF);
     maxRange = range; // Reset max range
   }
   else if (range > maxRange) {
@@ -230,7 +289,12 @@ void loop()
     minRange = range;
   }
 
+  // Ensure max range does not go above maximum allowed
+  if (maxRange > MAX_RANGE) {
+    maxRange = MAX_RANGE;
+  }
+
   oled.printf("%4d %4d %4d %6d\r", minRange, range, maxRange, count);
-  oled.invertDisplay(tripped);
+  //oled.invertDisplay(tripped);
 }
 
